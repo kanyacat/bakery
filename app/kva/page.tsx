afd1d379f7567988bf3cf4a4a808e6a93a36a6e8
kan <kanyakancat@gmail.com>
2024-03-31 15:01:28 +0300

    init

---
+++ app/kva/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => (
+    <div>kva</div>
+);
+
+export default Page;
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import {classNames} from "@/app/lib/classNames/classNames";
+
+export default function Home() {
+    return (
+        <div className={classNames('', {}, [])}>'Home'</div>)
+}
